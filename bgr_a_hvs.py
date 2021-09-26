14bbb719cae5cad0f55c59d42a386ff38552571f
abrahambtz <abrahambz@outlook.com>
2021-09-26 13:23:33 -0500

    Converison de BGR a HSV.
    Se realizan modificacione en el codigo para solicitar los valores BGR.

--- bgr_a_hvs.py
+++ bgr_a_hvs.py
@@ -3,7 +3,11 @@ import cv2
 
 
 def run():
-    colorBGR = np.uint8([[[255,0,0]]])
+    
+    b = input('B: 0-255: ')
+    g = input('G: 0-255: ')
+    r = input('R: 0-255: ')
+    colorBGR = np.uint8([[[b, g, r]]])
     colorHVS = cv2.cvtColor(colorBGR, cv2.COLOR_BGR2HSV)
     print(colorHVS)
 
